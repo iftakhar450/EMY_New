a3713db6768597c49d4bd0f3db13a1b9e29c00a8
iftakhar450 <iftakhar167@gmail.com>
2019-11-08 23:00:03 +0400

    heroku mongo config added

--- config/mysqlconfig.js
+++ config/mysqlconfig.js
@@ -4,8 +4,13 @@
  * mongodb connection config
  */
 module.exports = {
-    host: process.env.MYSQL_URI || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "changeme",
-    database: process.env.MYSQL_DATABASE || "emy",
+    //mysql://bfa8ab275f223a:61311f5e@us-cdbr-iron-east-05.cleardb.net/heroku_a252573ffce18d2?reconnect=true
+    // host: process.env.MYSQL_URI || "localhost",
+    // user: process.env.MYSQL_USER || "root",
+    // password: process.env.MYSQL_PASSWORD || "changeme",
+    // database: process.env.MYSQL_DATABASE || "emy",
+    host: process.env.MYSQL_URI || "us-cdbr-iron-east-05.cleardb.net",
+    user: process.env.MYSQL_USER || "bfa8ab275f223a",
+    password: process.env.MYSQL_PASSWORD || "61311f5e",
+    database: process.env.MYSQL_DATABASE || "heroku_a252573ffce18d2",
 };

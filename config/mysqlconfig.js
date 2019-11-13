8c0c2b4496c1e81f49eb13034fce5bd32f5f312d
iftakhar450 <iftakhar167@gmail.com>
2019-11-13 20:49:59 +0400

    db config addded

--- config/mysqlconfig.js
+++ config/mysqlconfig.js
@@ -4,13 +4,13 @@
  * mongodb connection config
  */
 module.exports = {
-    //mysql://bfa8ab275f223a:61311f5e@us-cdbr-iron-east-05.cleardb.net/heroku_a252573ffce18d2?reconnect=true
+    //mysql://bc032250518b90:4263e4b3@us-cdbr-iron-east-05.cleardb.net/heroku_3dbfaeedd580aea?reconnect=true
     // host: process.env.MYSQL_URI || "localhost",
     // user: process.env.MYSQL_USER || "root",
     // password: process.env.MYSQL_PASSWORD || "changeme",
     // database: process.env.MYSQL_DATABASE || "emy",
     host: process.env.MYSQL_URI || "us-cdbr-iron-east-05.cleardb.net",
-    user: process.env.MYSQL_USER || "bfa8ab275f223a",
-    password: process.env.MYSQL_PASSWORD || "61311f5e",
-    database: process.env.MYSQL_DATABASE || "heroku_a252573ffce18d2",
+    user: process.env.MYSQL_USER || "bc032250518b90",
+    password: process.env.MYSQL_PASSWORD || "4263e4b3",
+    database: process.env.MYSQL_DATABASE || "heroku_3dbfaeedd580aea",
 };
